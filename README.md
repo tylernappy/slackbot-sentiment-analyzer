7d7f45c9d288e8b30ed02ae1c93805a98c67c84a
tylernappy <tylernappy@gmail.com>
2016-08-01 15:49:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Slackbot using Haven OnDemand Sentiment Analysis and Botkit
+A simple example to show how to incorporate Haven OnDemand into a Slackbot.
+
+## What this does
+Prompt the Slackbot in the slack channel with a sentence and it will analyze the sentiment of it.
+
+![workflow](./workflow.png)
+
+## Technology used
+* [Haven OnDemand](http://havenondemand.com/)
+* [Slackbots](https://github.com/howdyai/botkit/blob/master/readme-slack.md)
+* [Node.js](https://nodejs.org/en/)
+* [Botkit](https://howdy.ai/botkit/)
---
+++ index.js
@@ -0,0 +1,31 @@
+var env = process.env.NODE_ENV || 'dev'
+if (env == 'dev') {
+  require('dotenv').load()
+}
+
+// Define client for Haven OnDemand
+var havenondemand = require('havenondemand')
+var  client = new havenondemand.HODClient(process.env.HOD_APIKEY)
+
+//Define Botkit controller
+var Botkit = require('Botkit')
+
+var controller = Botkit.slackbot({
+    debug: false
+})
+
+var bot = controller.spawn({
+    token: process.env.SLACK_TOKEN
+}).startRTM()
+
+// If someone directly mentions the bot
+controller.hears(['analyze sentiment of (.*)'], 'direct_mention', function(bot, message) {
+  var text = message.match[1] //Extract text from Slack message
+  var data = {text: text}
+  client.post('analyzesentiment', data, function(err, resp, body) {
+    var overallSentiment = resp.body.aggregate.sentiment // Get overall sentiment
+    var overallScore = resp.body.aggregate.score // Get overall Score
+    var replyText = "Finished analyzing *'" + text +  "'*\nThe overall sentiment is *" + overallSentiment + "*\nThe overall score is *" + overallScore + "*" // Format reply text
+    bot.reply(message, replyText) // Reply with information
+  })
+})
